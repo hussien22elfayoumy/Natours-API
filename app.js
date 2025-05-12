d1b83361c3590728a93dbe0506a03bbe729ea6c0
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-05-12 22:18:34 +0300

    simple api request

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 import express from 'express';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __fileName = fileURLToPath(import.meta.url);
+const __dirName = dirname(__fileName);
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello from server side' });
+app.use(express.json());
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirName}/dev-data/data/tours-simple.json`, 'utf-8')
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    message: 'sucess',
+    data: {
+      tours: tours,
+    },
+  });
 });
 
 const PORT = 8000;
