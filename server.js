a66cc18db1f855d38ac1691137e99ff4aff0f293
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-05-14 18:29:10 +0300

    make our first model

--- server.js
+++ server.js
@@ -6,6 +6,30 @@ mongoose
   .then(() => console.log('connectd to mongodb'))
   .catch((err) => console.log(err));
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+const testTour = new Tour({
+  name: 'hello again',
+  rating: 4.5,
+  price: 444,
+});
+
+testTour.save();
+
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
