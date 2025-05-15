7830cf737cf27572421aafc36d1f7eb68465638d
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-05-15 20:13:40 +0300

    update and deleting the tours

--- src/controllers/tourController.js
+++ src/controllers/tourController.js
@@ -55,16 +55,42 @@ export const getTour = async (req, res) => {
   }
 };
 
-export const updateTour = (req, res) => {
+export const updateTour = async (req, res) => {
+  try {
+    const updatedTour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: updatedTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent!',
+    });
+  }
   res.status(500).json({
     status: 'error',
     message: 'This route is not yet defiend',
   });
 };
 
-export const deleteTour = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defiend',
-  });
+export const deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      message: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
