a9a07ea3144b522ec8ecb907c83b6fae09c6d32c
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-05-20 14:34:14 +0300

    implementing global error handling middleware

--- src/app.js
+++ src/app.js
@@ -25,9 +25,27 @@ app.use('/api/v1/users', userRouter);
 // handle unhandled routes
 
 app.all('/{*any}', (req, res, next) => {
-  res.status(404).json({
+  /* res.status(404).json({
     status: 'fail',
     message: `Can't get the route ${req.originalUrl}`,
+  }); */
+
+  const err = new Error(`Can't get the route ${req.originalUrl}`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err); // will pass it to next middleware in middleware stack
+});
+
+// global error middleware
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 
   next();
