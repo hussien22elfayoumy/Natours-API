ad518cd35e9e66ac449bb2442ad8d97cefd54795
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-05-20 17:39:28 +0300

    errors outside express unhandled rejections

--- src/server.js
+++ src/server.js
@@ -3,11 +3,19 @@ import app from './app.js';
 
 mongoose
   .connect(process.env.DB_URL)
-  .then(() => console.log('connectd to mongodb'))
-  .catch((err) => console.log(err));
+  .then(() => console.log('connectd to mongodb'));
+// .catch((err) => console.log(err));
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`App is running on port, ${PORT}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('unhandled rejections, Shutting down the system');
+
+  // process.exit(0) => success || process.exit(1) => Failure / Error
+  server.close(() => process.exit(1));
 });
