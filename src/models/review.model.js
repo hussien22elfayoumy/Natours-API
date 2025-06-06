37fb5a5efc75ebd1e88ba6674eda10ce24aee1c5
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-06-06 19:23:11 +0300

    populating reviews with tours and users

--- src/models/review.model.js
+++ src/models/review.model.js
@@ -28,6 +28,19 @@ const reviewSchema = new mongoose.Schema(
   },
 );
 
+// Populate tour and user before reterving reviews
+reviewSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'tour',
+    select: 'name',
+  }).populate({
+    path: 'user',
+    select: 'name photo',
+  });
+
+  next();
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 
 export default Review;
