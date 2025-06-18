9cd5282476b8c8f44db555029bd83e778d13c13a
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-06-18 21:46:25 +0300

    building a complex email handler

--- src/utils/send-email.js
+++ src/utils/send-email.js
@@ -1,27 +1,61 @@
 import nodemailer from 'nodemailer';
+import pug from 'pug';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import htmlToText from 'html-to-text';
 
-const sendEmail = async (options) => {
+const _fileName = fileURLToPath(import.meta.url);
+const _dirname = dirname(_fileName);
+
+export default class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Hussien Mohammed <${process.env.EMAIL_FROM}>`;
+  }
+
   // 1) Create a transporter
+  newTransporter() {
+    if (process.env.NODE_ENV === 'production') {
+      // use Sandgrid
+      return 1;
+    }
 
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
 
-  // 2) define the email options
-  const mailOptions = {
-    from: 'Hussien MOhammed <admin@email.com',
-    to: options.email,
-    subject: options.subject,
-    text: options.text,
-  };
+  // 2) Send actual email
+  async send(template, subject) {
+    // 1) Render HTML based on a pug template (generate)
+    const html = pug.render(`${_dirname}/../views/emails/${template}.pug`, {
+      firstName: this.firstName,
+      subject: this.subject,
+      url: this.url,
+    });
 
-  // 3) send the actual email
-  await transporter.sendMail(mailOptions);
-};
+    // 2) define the mail options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.convert(html),
+    };
 
-export default sendEmail;
+    // 3) Create a transport and send email
+    await this.newTransporter().sendMail(mailOptions);
+  }
+
+  // 3) Custom email
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to the Natuors family.');
+  }
+}
